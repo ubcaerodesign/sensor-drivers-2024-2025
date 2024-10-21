edff50d3ceb2577e3363bb147bb7612cb12e4700
annascao <anna.shuqi@gmail.com>
2024-10-20 23:16:21 -0700

    upload BNO055 driver files

---
+++ BNO055.c
@@ -0,0 +1,459 @@
+#include "BNO055.h"
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+I2C_HandleTypeDef *BNO055_i2c_port;
+
+uint16_t accelScale = 100;
+uint16_t tempScale = 1;
+uint16_t angularRateScale = 16;
+uint16_t eulerScale = 16;
+uint16_t magScale = 16;
+uint16_t quaScale = (1<<14); // 2^14
+
+double theta_F_old = 0, phi_F_old = 0;
+double theta = 0, phi = 0;
+
+
+/**
+ * @brief Configures which i2c port bno055 is on
+ */
+void BNO055_assignI2C(I2C_HandleTypeDef *hi2c_device) {
+	BNO055_i2c_port = hi2c_device;
+}
+
+/**
+ * @brief Write to sensor
+ * note: i2c HAL API automatically handles the I2C write/read bit for you.
+ */
+void BNO055_writeRegister(uint8_t reg, uint8_t data) {
+  uint8_t txdata[2] = {reg, data};
+  uint8_t status = HAL_I2C_Master_Transmit(BNO055_i2c_port, BNO055_I2C_ADDR << 1, txdata, sizeof(txdata), HAL_MAX_DELAY);
+  //HAL_I2C_Mem_Write(BNO055_i2c_port, BNO055_I2C_ADDR << 1, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+
+  // Debugging
+    if (status == HAL_OK) {
+      return;
+    }
+
+    if (status == HAL_ERROR) {
+      printf("HAL_I2C_Master_Transmit HAL_ERROR\r\n");
+    } else if (status == HAL_TIMEOUT) {
+      printf("HAL_I2C_Master_Transmit HAL_TIMEOUT\r\n");
+    } else if (status == HAL_BUSY) {
+      printf("HAL_I2C_Master_Transmit HAL_BUSY\r\n");
+    } else {
+      printf("Unknown status data %d", status);
+    }
+
+    uint32_t error = HAL_I2C_GetError(BNO055_i2c_port);
+    if (error == HAL_I2C_ERROR_NONE) {
+      return;
+    } else if (error == HAL_I2C_ERROR_BERR) {
+      printf("HAL_I2C_ERROR_BERR\r\n");
+    } else if (error == HAL_I2C_ERROR_ARLO) {
+      printf("HAL_I2C_ERROR_ARLO\r\n");
+    } else if (error == HAL_I2C_ERROR_AF) {
+      printf("HAL_I2C_ERROR_AF\r\n");
+    } else if (error == HAL_I2C_ERROR_OVR) {
+      printf("HAL_I2C_ERROR_OVR\r\n");
+    } else if (error == HAL_I2C_ERROR_DMA) {
+      printf("HAL_I2C_ERROR_DMA\r\n");
+    } else if (error == HAL_I2C_ERROR_TIMEOUT) {
+      printf("HAL_I2C_ERROR_TIMEOUT\r\n");
+    }
+}
+
+/**
+ * @brief Read from sensor
+ *  note: i2c HAL API automatically handles the I2C write/read bit for you.
+ */
+void BNO055_readRegister(uint8_t reg, uint8_t *data, uint8_t len) {
+
+  HAL_I2C_Mem_Read(BNO055_i2c_port, BNO055_I2C_ADDR << 1, reg, I2C_MEMADD_SIZE_8BIT, data, len, HAL_MAX_DELAY);
+
+}
+
+void BNO055_setPage(uint8_t page) { BNO055_writeRegister(BNO055_PAGE_ID, page); }
+
+void BNO055_reset() {
+  BNO055_writeRegister(BNO055_SYS_TRIGGER, 0x20);
+  HAL_Delay(700);
+}
+
+void BNO055_setExternalCrystalUse(bool state) {
+  BNO055_setPage(0);
+  uint8_t tmp = 0;
+  BNO055_readRegister(BNO055_SYS_TRIGGER, &tmp, 1);
+  tmp |= (state == true) ? 0x80 : 0x0;
+  BNO055_writeRegister(BNO055_SYS_TRIGGER, tmp);
+}
+
+BNO055_opmode_t BNO055_getOperationMode() {
+  BNO055_opmode_t mode;
+  BNO055_readRegister(BNO055_OPR_MODE, &mode, 1);
+  return mode;
+}
+
+void BNO055_setOperationMode(BNO055_opmode_t mode) {
+  BNO055_writeRegister(BNO055_OPR_MODE, mode);
+  if (mode == OPERATION_MODE_CONFIG) {
+    HAL_Delay(19);
+  } else {
+	HAL_Delay(7);
+  }
+}
+
+void BNO055_setOperationModeConfig() {
+  BNO055_setOperationMode(OPERATION_MODE_CONFIG);
+}
+
+void BNO055_setOperationModeNDOF() {
+  BNO055_setOperationMode(OPERATION_MODE_NDOF);
+}
+
+void BNO055_setAxisRemap(BNO055_axis_remap_config_t remapcode) {
+  BNO055_setOperationModeConfig();
+  BNO055_writeRegister(BNO055_AXIS_MAP_CONFIG, remapcode);
+  BNO055_setOperationModeNDOF();
+}
+
+void BNO055_setAxisSign(BNO055_axis_remap_sign_t remapsign) {
+  BNO055_setOperationModeConfig();
+  BNO055_writeRegister(BNO055_AXIS_MAP_SIGN, remapsign);
+  BNO055_setOperationModeNDOF();
+}
+
+
+void BNO055_setup() {
+  BNO055_reset();
+
+  uint8_t id = 0;
+  BNO055_readRegister(BNO055_CHIP_ID, &id, 1);
+  if (id != BNO055_ID) {
+    printf("Can't find BNO055, id: 0x%02x. Please check your wiring.\r\n", id);
+  }
+  BNO055_setOperationModeConfig();
+
+  BNO055_writeRegister(BNO055_PWR_MODE, POWER_MODE_NORMAL);
+  BNO055_setPage(0);
+  BNO055_writeRegister(BNO055_SYS_TRIGGER, 0x0);
+
+  //BNO055_setOperationModeConfig();
+  BNO055_setOperationModeNDOF();
+}
+
+bool BNO055_getCalibrationState(BNO055_calibration_state_t *calState) {
+  BNO055_setPage(0);
+  uint8_t cal = 0;
+
+  BNO055_readRegister(BNO055_CALIB_STAT, &cal, 1);
+
+  calState->sys = (cal >> 6) & 0x03;
+  calState->gyro = (cal >> 4) & 0x03;
+  calState->accel = (cal >> 2) & 0x03;
+  calState->mag = cal & 0x03;
+
+  if((calState->sys == 3) && (calState->gyro == 3) && (calState->accel == 3) && (calState->mag == 3)){
+	  return true;
+  }
+  else{
+	  return false;
+  }
+
+}
+
+void BNO055_getCalibrationData(BNO055_offsets_t *calData, BNO055_calibration_state_t *calState) {
+	uint8_t buffer[22];
+
+	if(BNO055_getCalibrationState(calState)){ //double check that bno is fully calibrated
+
+		BNO055_setOperationModeConfig();
+
+		BNO055_readRegister(BNO055_ACC_OFFSET_X_LSB, buffer, 22);
+
+		calData->accel_offset_x = ((buffer[1] << 8) | buffer[0]);
+		calData->accel_offset_y = ((buffer[3] << 8) | buffer[2]);
+		calData->accel_offset_z = ((buffer[5] << 8) | buffer[4]);
+
+		calData->mag_offset_x = ((buffer[7] << 8) | buffer[6]);
+		calData->mag_offset_y = ((buffer[9] << 8) | buffer[8]);
+		calData->mag_offset_z = ((buffer[11] << 8) | buffer[10]);
+
+		calData->gyro_offset_x = ((buffer[13] << 8) | buffer[12]);
+		calData->gyro_offset_y = ((buffer[15] << 8) | buffer[14]);
+		calData->gyro_offset_z = ((buffer[17] << 8) | buffer[16]);
+
+		calData->accel_radius = ((buffer[19] << 8) | buffer[18]);
+
+		calData->mag_radius = ((buffer[21] << 8) | buffer[20]);
+
+		BNO055_setOperationModeNDOF();
+
+	}
+
+}
+
+void BNO055_setCalibrationData(BNO055_offsets_t *calData) {
+	uint8_t buffer[22];
+	memcpy(buffer, calData, 22);
+
+	BNO055_setOperationModeConfig();
+
+	for (uint8_t i=0; i < 22; i++) {
+	    BNO055_writeRegister(BNO055_ACC_OFFSET_X_LSB+i, buffer[i]);
+	}
+
+	BNO055_setOperationModeNDOF();
+}
+
+void BNO055_displayCalibrationData(BNO055_offsets_t *calData){
+    printf("Accel: ");
+    printf("%d, %d, %d \r\n", calData->accel_offset_x,calData->accel_offset_y, calData->accel_offset_z);
+
+    printf("Gyro: ");
+    printf("%d, %d, %d \r\n", calData->gyro_offset_x,calData->gyro_offset_y, calData->gyro_offset_z);
+
+    printf("Mag: ");
+    printf("%d, %d, %d \r\n", calData->mag_offset_x,calData->mag_offset_y, calData->mag_offset_z);
+
+    printf("Accel Radius: ");
+    printf("%d \r\n", calData->accel_radius);
+
+    printf("Mag Radius: ");
+    printf("%d \r\n", calData->mag_radius);
+
+    HAL_Delay(500);
+
+}
+
+void BNO055_calibrationRoutine(){
+	BNO055_calibration_state_t calState;
+	BNO055_offsets_t calData;
+
+	BNO055_setPage(0);
+
+	while(! BNO055_getCalibrationState(&calState)){
+		printf("sys = %d, gyro = %d, mag = %d, accel = %d \r\n", calState.sys, calState.gyro, calState.mag, calState.accel);
+		HAL_Delay(500);
+
+	}
+	printf("--------CALIBRATION COMPLETE STORING INTO EEPROM--------\r\n");
+	BNO055_getCalibrationData(&calData, &calState);
+	BNO055_displayCalibrationData(&calData);
+	BNO055_setCalibrationData(&calData);
+	printf("--------DONE--------\r\n");
+
+}
+
+void BNO055_getVector(uint8_t vec, BNO055_vector_t *xyz) {
+	BNO055_setPage(0);
+	uint8_t buffer[8];    // Quaternion needs 8 bytes
+
+	if (vec == BNO055_VECTOR_QUATERNION)
+		BNO055_readRegister(vec, buffer, 8);
+	else
+		BNO055_readRegister(vec, buffer, 6);
+
+	double scale = 1;
+
+	if (vec == BNO055_VECTOR_MAGNETOMETER) {
+		scale = magScale;
+	} else if (vec == BNO055_VECTOR_ACCELEROMETER ||
+		   vec == BNO055_VECTOR_LINEARACCEL || vec == BNO055_VECTOR_GRAVITY) {
+		scale = accelScale;
+	} else if (vec == BNO055_VECTOR_GYROSCOPE) {
+		scale = angularRateScale;
+	} else if (vec == BNO055_VECTOR_EULER) {
+		scale = eulerScale;
+	} else if (vec == BNO055_VECTOR_QUATERNION) {
+		scale = quaScale;
+	}
+
+	if (vec == BNO055_VECTOR_QUATERNION) {
+		xyz->w = (int16_t)((buffer[1] << 8) | buffer[0]) / scale;
+		xyz->x = (int16_t)((buffer[3] << 8) | buffer[2]) / scale;
+		xyz->y = (int16_t)((buffer[5] << 8) | buffer[4]) / scale;
+		xyz->z = (int16_t)((buffer[7] << 8) | buffer[6]) / scale;
+	} else {
+		xyz->x = (int16_t)((buffer[1] << 8) | buffer[0]) / scale;
+		xyz->y = (int16_t)((buffer[3] << 8) | buffer[2]) / scale;
+		xyz->z = (int16_t)((buffer[5] << 8) | buffer[4]) / scale;
+	}
+}
+
+void BNO055_getVectorAccelerometer(BNO055_vector_t *xyz) {
+	BNO055_getVector(BNO055_VECTOR_ACCELEROMETER, xyz);
+}
+void BNO055_getVectorMagnetometer(BNO055_vector_t *xyz) {
+	BNO055_getVector(BNO055_VECTOR_MAGNETOMETER, xyz);
+}
+void BNO055_getVectorGyroscope(BNO055_vector_t *xyz) {
+	BNO055_getVector(BNO055_VECTOR_GYROSCOPE, xyz);
+}
+void BNO055_getVectorEuler(BNO055_vector_t *xyz) {
+	BNO055_getVector(BNO055_VECTOR_EULER, xyz);
+}
+void BNO055_getVectorLinearAccel(BNO055_vector_t *xyz) {
+	BNO055_getVector(BNO055_VECTOR_LINEARACCEL, xyz);
+}
+void BNO055_getVectorGravity(BNO055_vector_t *xyz) {
+	BNO055_getVector(BNO055_VECTOR_GRAVITY, xyz);
+}
+void BNO055_getVectorQuaternion(BNO055_vector_t *xyz) {
+	BNO055_getVector(BNO055_VECTOR_QUATERNION, xyz);
+}
+
+void BNO055_getVectorEuler_fromVectorQuaternion(BNO055_vector_t *quaternion,BNO055_vector_t *euler){
+	BNO055_vector_t convertedQuaternion;
+	BNO055_vector_t changeQuaternion = {.w = 0.5, .x = 0.5, .y = 0.5, .z = 0.5};
+
+	BNO055_multiplyQuaternion(&changeQuaternion, quaternion, &convertedQuaternion);
+
+    double sq_w = convertedQuaternion.w * convertedQuaternion.w;
+    double sq_x = convertedQuaternion.x * convertedQuaternion.x;
+    double sq_y = convertedQuaternion.y * convertedQuaternion.y;
+    double sq_z = convertedQuaternion.z * convertedQuaternion.z;
+
+    double unit = sq_w + sq_x + sq_y + sq_z;
+    double test = convertedQuaternion.x * convertedQuaternion.y + convertedQuaternion.z * convertedQuaternion.w;
+
+    /** x = roll, y = pitch, z = yaw **/
+    euler->x = atan2(2 * (convertedQuaternion.w * convertedQuaternion.x + convertedQuaternion.y * convertedQuaternion.z), sq_y + sq_w - sq_x - sq_z);
+    euler->y = atan2(2 * (convertedQuaternion.w * convertedQuaternion.y - convertedQuaternion.x * convertedQuaternion.z), sq_w + sq_x - sq_y - sq_z);
+
+    // initial conditionals are for catching singularities when converting to Euler
+    if (test > 0.4999 * unit) {
+    	euler->z = M_PI / 2;
+    }
+    else if (test < -0.4999 * unit) {
+    	euler->z = -M_PI / 2;
+    }
+    else {
+    	euler->z = asin(2 * test / unit);
+    }
+
+}
+
+void BNO055_getMagneticHeading
+(BNO055_vector_t *refQuaternion, BNO055_vector_t *refGravity, BNO055_vector_t *quaternion, double *heading){
+
+	double theta_M, phi_M;
+	double theta_F_new, phi_F_new;
+	double Xm, Ym;
+
+	double rot_matrix[3][3] = {
+	        {0.0, 0.0, 0.0},
+	        {0.0, 0.0, 0.0},
+	        {0.0, 0.0, 0.0}};
+
+	BNO055_vector_t diffQuaternion, newGravity, magnetometer;
+
+	BNO055_multiplyQuaternion(refQuaternion, quaternion, &diffQuaternion);
+	BNO055_quaternionToMatrix(&diffQuaternion, rot_matrix);
+	BNO055_inverseMatrix(rot_matrix);
+	BNO055_matrixVectorMultiply(rot_matrix, refGravity, &newGravity);
+
+	BNO055_getVectorMagnetometer(&magnetometer);
+
+	theta_M = atan2(newGravity.x / GRAVITY, newGravity.z / GRAVITY);
+	phi_M = atan2(newGravity.y / GRAVITY, newGravity.z / GRAVITY);
+	phi_F_new = 0.95 * phi_F_old + 0.05 * phi_M;
+	theta_F_new = 0.95 * theta_F_old + 0.05 * theta_M;
+
+	theta = theta * 0.95 + theta_M * 0.05;
+	phi = phi * 0.95 + phi_M * 0.05;
+
+	Xm = magnetometer.x * cos(theta) - magnetometer.y * sin(phi) * sin(theta) + magnetometer.z * cos(phi) * sin(theta);
+	Ym = magnetometer.y * cos(phi) + magnetometer.z * sin(phi);
+
+	*heading = atan2(Ym, Xm) * 180 / M_PI;
+
+	theta_F_old = theta_F_new;
+	phi_F_old = phi_F_new;
+
+	if (*heading < 0) {
+		*heading += 360;
+	}
+
+}
+
+void BNO055_getReferences(BNO055_vector_t *refQuaternion, BNO055_vector_t *refGravity){
+	BNO055_vector_t initialAccel, newAccel;
+
+	BNO055_getVectorAccelerometer(&initialAccel);
+
+	unsigned long start_test = HAL_GetTick();
+
+	// checks if IMU is stationary within defined test duration
+	while ( HAL_GetTick() - start_test <= ACCEL_SYNC_TIME ) {
+		BNO055_getVectorAccelerometer(&newAccel);
+
+		// checks if acceleration components exceed defined threshold
+		if ( !( abs(newAccel.x - initialAccel.x) < ACCEL_SYNC_THRESHOLD &&
+				abs(newAccel.y - initialAccel.y) < ACCEL_SYNC_THRESHOLD &&
+				abs(newAccel.z - initialAccel.z) < ACCEL_SYNC_THRESHOLD )) {
+
+			// update start time and initial acceleration
+			BNO055_getVectorAccelerometer(&initialAccel);
+			start_test = HAL_GetTick();
+		}
+	}
+
+	// get reference gravity vector and quaternion
+	BNO055_getVectorAccelerometer(refGravity);
+	BNO055_getVectorQuaternion(refQuaternion);
+	BNO055_inverseQuaternion(refQuaternion);
+
+}
+
+void BNO055_inverseQuaternion(BNO055_vector_t *q) { //is this right lol
+	q->w = q->w;
+	q->x = -(q->x);
+	q->y = -(q->y);
+	q->z = -(q->z);
+}
+
+void BNO055_multiplyQuaternion(BNO055_vector_t *q1, BNO055_vector_t *q2, BNO055_vector_t *q3) {
+	 q3->w = q1->w * q2->w - q1->x * q2->x - q1->y * q2->y - q1->z * q2->z;
+	 q3->x = q1->w * q2->x + q1->x * q2->w + q1->y * q2->z - q1->z * q2->y;
+	 q3->y = q1->w * q2->y - q1->x * q2->z + q1->y * q2->w + q1->z * q2->x;
+	 q3->z = q1->w * q2->z + q1->x * q2->y - q1->y * q2->x + q1->z * q2->w;
+}
+
+void BNO055_inverseMatrix(double matrix[3][3]){
+	double temp;
+
+	// Swap elements across the diagonal
+	for (int i = 0; i < 3; i++) {
+		for (int j = i + 1; j < 3; j++) {
+			// Swap matrix[i][j] and matrix[j][i]
+			temp = matrix[i][j];
+			matrix[i][j] = matrix[j][i];
+			matrix[j][i] = temp;
+		}
+	}
+}
+
+void BNO055_quaternionToMatrix(BNO055_vector_t *q, double matrix[3][3]) {
+    matrix[0][0] = 2 * (q->w * q->w + q->x * q->x) - 1;
+    matrix[0][1] = 2 * (q->x * q->y - q->w * q->z);
+    matrix[0][2] = 2 * (q->x * q->z + q->w * q->y);
+    matrix[1][0] = 2 * (q->x * q->y + q->w * q->z);
+    matrix[1][1] = 2 * (q->w * q->w + q->y * q->y) - 1;
+    matrix[1][2] = 2 * (q->y * q->z - q->w * q->x);
+    matrix[2][0] = 2 * (q->x * q->z - q->w * q->y);
+    matrix[2][1] = 2 * (q->y * q->z + q->w * q->x);
+    matrix[2][2] = 2 * (q->w * q->w + q->z * q->z) - 1;
+}
+
+void BNO055_matrixVectorMultiply(double matrix[3][3], BNO055_vector_t *refGravity, BNO055_vector_t *newGravity) {
+	newGravity->x = matrix[0][0] * refGravity->x + matrix[0][1] * refGravity->y + matrix[0][2] * refGravity->z;
+	newGravity->y = matrix[1][0] * refGravity->x + matrix[1][1] * refGravity->y + matrix[1][2] * refGravity->z;
+	newGravity->z = matrix[2][0] * refGravity->x + matrix[2][1] * refGravity->y + matrix[2][2] * refGravity->z;
+
+}
+
+
---
+++ BNO055.h
@@ -0,0 +1,269 @@
+#ifndef BNO055_H
+#define BNO055_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f1xx_hal.h" /* Needed for I2C */
+
+#define ACCEL_SYNC_TIME 500
+#define ACCEL_SYNC_THRESHOLD 0.15
+#define GRAVITY 9.80665
+
+#define BNO055_I2C_ADDR_HI 0x29
+#define BNO055_I2C_ADDR_LO 0x28
+#define BNO055_I2C_ADDR    BNO055_I2C_ADDR_HI
+
+/**
+ * Registers
+ */
+#define BNO055_ID (0xA0)
+#define BNO055_CHIP_ID 0x00
+#define BNO055_ACC_ID 0x01
+#define BNO055_MAG_ID 0x02
+#define BNO055_GYRO_ID 0x03
+#define BNO055_SW_REV_ID_LSB 0x04
+#define BNO055_SW_REV_ID_MSB 0x05
+#define BNO055_BL_REV_ID 0x06
+#define BNO055_PAGE_ID 0x07
+#define BNO055_ACC_DATA_X_LSB 0x08
+#define BNO055_ACC_DATA_X_MSB 0x09
+#define BNO055_ACC_DATA_Y_LSB 0x0A
+#define BNO055_ACC_DATA_Y_MSB 0x0B
+#define BNO055_ACC_DATA_Z_LSB 0x0C
+#define BNO055_ACC_DATA_Z_MSB 0x0D
+#define BNO055_MAG_DATA_X_LSB 0x0E
+#define BNO055_MAG_DATA_X_MSB 0x0F
+#define BNO055_MAG_DATA_Y_LSB 0x10
+#define BNO055_MAG_DATA_Y_MSB 0x11
+#define BNO055_MAG_DATA_Z_LSB 0x12
+#define BNO055_MAG_DATA_Z_MSB 0x13
+#define BNO055_GYR_DATA_X_LSB 0x14
+#define BNO055_GYR_DATA_X_MSB 0x15
+#define BNO055_GYR_DATA_Y_LSB 0x16
+#define BNO055_GYR_DATA_Y_MSB 0x17
+#define BNO055_GYR_DATA_Z_LSB 0x18
+#define BNO055_GYR_DATA_Z_MSB 0x19
+#define BNO055_EUL_HEADING_LSB 0x1A
+#define BNO055_EUL_HEADING_MSB 0x1B
+#define BNO055_EUL_ROLL_LSB 0x1C
+#define BNO055_EUL_ROLL_MSB 0x1D
+#define BNO055_EUL_PITCH_LSB 0x1E
+#define BNO055_EUL_PITCH_MSB 0x1F
+#define BNO055_QUA_DATA_W_LSB 0x20
+#define BNO055_QUA_DATA_W_MSB 0x21
+#define BNO055_QUA_DATA_X_LSB 0x22
+#define BNO055_QUA_DATA_X_MSB 0x23
+#define BNO055_QUA_DATA_Y_LSB 0x24
+#define BNO055_QUA_DATA_Y_MSB 0x25
+#define BNO055_QUA_DATA_Z_LSB 0x26
+#define BNO055_QUA_DATA_Z_MSB 0x27
+#define BNO055_LIA_DATA_X_LSB 0x28
+#define BNO055_LIA_DATA_X_MSB 0x29
+#define BNO055_LIA_DATA_Y_LSB 0x2A
+#define BNO055_LIA_DATA_Y_MSB 0x2B
+#define BNO055_LIA_DATA_Z_LSB 0x2C
+#define BNO055_LIA_DATA_Z_MSB 0x2D
+#define BNO055_GRV_DATA_X_LSB 0x2E
+#define BNO055_GRV_DATA_X_MSB 0x2F
+#define BNO055_GRV_DATA_Y_LSB 0x30
+#define BNO055_GRV_DATA_Y_MSB 0x31
+#define BNO055_GRV_DATA_Z_LSB 0x32
+#define BNO055_GRV_DATA_Z_MSB 0x33
+#define BNO055_TEMP 0x34
+#define BNO055_CALIB_STAT 0x35
+#define BNO055_ST_RESULT 0x36
+#define BNO055_INT_STATUS 0x37
+#define BNO055_SYS_CLK_STATUS 0x38
+#define BNO055_SYS_STATUS 0x39
+#define BNO055_SYS_ERR 0x3A
+#define BNO055_UNIT_SEL 0x3B
+#define BNO055_OPR_MODE 0x3D
+#define BNO055_PWR_MODE 0x3E
+#define BNO055_SYS_TRIGGER 0x3F
+#define BNO055_TEMP_SOURCE 0x40
+#define BNO055_AXIS_MAP_CONFIG 0x41
+#define BNO055_AXIS_MAP_SIGN 0x42
+#define BNO055_ACC_OFFSET_X_LSB 0x55
+#define BNO055_ACC_OFFSET_X_MSB 0x56
+#define BNO055_ACC_OFFSET_Y_LSB 0x57
+#define BNO055_ACC_OFFSET_Y_MSB 0x58
+#define BNO055_ACC_OFFSET_Z_LSB 0x59
+#define BNO055_ACC_OFFSET_Z_MSB 0x5A
+#define BNO055_MAG_OFFSET_X_LSB 0x5B
+#define BNO055_MAG_OFFSET_X_MSB 0x5C
+#define BNO055_MAG_OFFSET_Y_LSB 0x5D
+#define BNO055_MAG_OFFSET_Y_MSB 0x5E
+#define BNO055_MAG_OFFSET_Z_LSB 0x5F
+#define BNO055_MAG_OFFSET_Z_MSB 0x60
+#define BNO055_GYR_OFFSET_X_LSB 0x61
+#define BNO055_GYR_OFFSET_X_MSB 0x62
+#define BNO055_GYR_OFFSET_Y_LSB 0x63
+#define BNO055_GYR_OFFSET_Y_MSB 0x64
+#define BNO055_GYR_OFFSET_Z_LSB 0x65
+#define BNO055_GYR_OFFSET_Z_MSB 0x66
+#define BNO055_ACC_RADIUS_LSB 0x67
+#define BNO055_ACC_RADIUS_MSB 0x68
+#define BNO055_MAG_RADIUS_LSB 0x69
+#define BNO055_MAG_RADIUS_MSB 0x6A
+
+#define BNO055_PAGE_ID 0x07
+#define BNO055_ACC_CONFIG 0x08
+#define BNO055_MAG_CONFIG 0x09
+#define BNO055_GYRO_CONFIG_0 0x0A
+#define BNO055_GYRO_CONFIG_1 0x0B
+#define BNO055_ACC_SLEEP_CONFIG 0x0C
+#define BNO055_GYR_SLEEP_CONFIG 0x0D
+#define BNO055_INT_MSK 0x0F
+#define BNO055_INT_EN 0x10
+#define BNO055_ACC_AM_THRES 0x11
+#define BNO055_ACC_INT_SETTINGS 0x12
+#define BNO055_ACC_HG_DURATION 0x13
+#define BNO055_ACC_HG_THRESH 0x14
+#define BNO055_ACC_NM_THRESH 0x15
+#define BNO055_ACC_NM_SET 0x16
+#define BNO055_GYR_INT_SETTINGS 0x17
+#define BNO055_GYR_HR_X_SET 0x18
+#define BNO055_GYR_DUR_X 0x19
+#define BNO055_GYR_HR_Y_SET 0x1A
+#define BNO055_GYR_DUR_Y 0x1B
+#define BNO055_GYR_HR_Z_SET 0x1C
+#define BNO055_GYR_DUR_Z 0x1D
+#define BNO055_GYR_AM_THRESH 0x1E
+#define BNO055_GYR_AM_SET 0x1F
+
+/** Operation Mode Settings **/
+typedef enum {
+  OPERATION_MODE_CONFIG = 0X00,
+  OPERATION_MODE_ACCONLY = 0X01,
+  OPERATION_MODE_MAGONLY = 0X02,
+  OPERATION_MODE_GYRONLY = 0X03,
+  OPERATION_MODE_ACCMAG = 0X04,
+  OPERATION_MODE_ACCGYRO = 0X05,
+  OPERATION_MODE_MAGGYRO = 0X06,
+  OPERATION_MODE_AMG = 0X07,
+  OPERATION_MODE_IMUPLUS = 0X08,
+  OPERATION_MODE_COMPASS = 0X09,
+  OPERATION_MODE_M4G = 0X0A,
+  OPERATION_MODE_NDOF_FMC_OFF = 0X0B,
+  OPERATION_MODE_NDOF = 0X0C
+} BNO055_opmode_t;
+
+/** BNO055 power settings */
+typedef enum {
+  POWER_MODE_NORMAL = 0X00,
+  POWER_MODE_LOWPOWER = 0X01,
+  POWER_MODE_SUSPEND = 0X02
+} BNO055_powermode_t;
+
+/** Remap settings **/
+typedef enum {
+  REMAP_CONFIG_P0 = 0x21,
+  REMAP_CONFIG_P1 = 0x24, // default
+  REMAP_CONFIG_P2 = 0x24,
+  REMAP_CONFIG_P3 = 0x21,
+  REMAP_CONFIG_P4 = 0x24,
+  REMAP_CONFIG_P5 = 0x21,
+  REMAP_CONFIG_P6 = 0x21,
+  REMAP_CONFIG_P7 = 0x24
+} BNO055_axis_remap_config_t;
+
+/** Remap Signs **/
+typedef enum {
+  REMAP_SIGN_P0 = 0x04,
+  REMAP_SIGN_P1 = 0x00, // default
+  REMAP_SIGN_P2 = 0x06,
+  REMAP_SIGN_P3 = 0x02,
+  REMAP_SIGN_P4 = 0x03,
+  REMAP_SIGN_P5 = 0x01,
+  REMAP_SIGN_P6 = 0x07,
+  REMAP_SIGN_P7 = 0x05
+} BNO055_axis_remap_sign_t;
+
+/** Type of vector(data) you want **/
+typedef enum {
+  BNO055_VECTOR_ACCELEROMETER = 0x08,  // Default: m/s²
+  BNO055_VECTOR_MAGNETOMETER = 0x0E,   // Default: uT
+  BNO055_VECTOR_GYROSCOPE = 0x14,      // Default: rad/s
+  BNO055_VECTOR_EULER = 0x1A,          // Default: degrees
+  BNO055_VECTOR_QUATERNION = 0x20,     // No units
+  BNO055_VECTOR_LINEARACCEL = 0x28,    // Default: m/s²
+  BNO055_VECTOR_GRAVITY = 0x2E         // Default: m/s²
+} BNO055_vector_type_t;
+
+/** Structure to represent offsets **/
+typedef struct {
+  int16_t accel_offset_x; /**< x acceleration offset */
+  int16_t accel_offset_y; /**< y acceleration offset */
+  int16_t accel_offset_z; /**< z acceleration offset */
+
+  int16_t mag_offset_x; /**< x magnetometer offset */
+  int16_t mag_offset_y; /**< y magnetometer offset */
+  int16_t mag_offset_z; /**< z magnetometer offset */
+
+  int16_t gyro_offset_x; /**< x gyroscrope offset */
+  int16_t gyro_offset_y; /**< y gyroscrope offset */
+  int16_t gyro_offset_z; /**< z gyroscrope offset */
+
+  int16_t accel_radius; /**< acceleration radius */
+
+  int16_t mag_radius; /**< magnetometer radius */
+} BNO055_offsets_t;
+
+/** Calibration States **/
+typedef struct {
+  uint8_t sys;
+  uint8_t gyro;
+  uint8_t mag;
+  uint8_t accel;
+} BNO055_calibration_state_t;
+
+/** Structure to store BNO055 sensor data **/
+typedef struct {
+  double w;
+  double x;
+  double y;
+  double z;
+} BNO055_vector_t;
+
+void BNO055_assignI2C(I2C_HandleTypeDef *hi2c_device);
+void BNO055_writeRegister(uint8_t reg, uint8_t data);
+void BNO055_readRegister(uint8_t reg, uint8_t *data, uint8_t len);
+
+void BNO055_setPage(uint8_t page);
+void BNO055_reset();
+void BNO055_setExternalCrystalUse(bool state);
+void BNO055_setOperationMode(BNO055_opmode_t mode);
+void BNO055_setOperationModeConfig();
+void BNO055_setOperationModeNDOF();
+void BNO055_setAxisRemap(BNO055_axis_remap_config_t remapcode);
+void BNO055_setAxisSign(BNO055_axis_remap_sign_t remapsign);
+void BNO055_setup();
+
+bool BNO055_getCalibrationState(BNO055_calibration_state_t *calState);
+void BNO055_getCalibrationData(BNO055_offsets_t *calData, BNO055_calibration_state_t *calState);
+void BNO055_setCalibrationData(BNO055_offsets_t *calData);
+void BNO055_displayCalibrationData(BNO055_offsets_t *calData);
+void BNO055_calibrationRoutine();
+
+void BNO055_getVector(uint8_t vec, BNO055_vector_t *xyz);
+void BNO055_getVectorAccelerometer(BNO055_vector_t *xyz);
+void BNO055_getVectorMagnetometer(BNO055_vector_t *xyz);
+void BNO055_getVectorGyroscope(BNO055_vector_t *xyz);
+void BNO055_getVectorEuler(BNO055_vector_t *xyz);
+void BNO055_getVectorLinearAccel(BNO055_vector_t *xyz);
+void BNO055_getVectorGravity(BNO055_vector_t *xyz);
+void BNO055_getVectorQuaternion(BNO055_vector_t *xyz);
+
+void BNO055_getVectorEuler_fromVectorQuaternion(BNO055_vector_t *quaternion,BNO055_vector_t *euler);
+void BNO055_getMagneticHeading
+(BNO055_vector_t *refQuaternion, BNO055_vector_t *refGravity, BNO055_vector_t *quaternion, double *heading);
+void BNO055_getReferences(BNO055_vector_t *refQuaternion, BNO055_vector_t *refGravity);
+
+void BNO055_inverseQuaternion(BNO055_vector_t *q);
+void BNO055_multiplyQuaternion(BNO055_vector_t *q1, BNO055_vector_t *q2, BNO055_vector_t *q3);
+void BNO055_inverseMatrix(double matrix[3][3]);
+void BNO055_quaternionToMatrix(BNO055_vector_t *q, double matrix[3][3]);
+void BNO055_matrixVectorMultiply(double matrix[3][3], BNO055_vector_t *refGravity, BNO055_vector_t *newGravity);
+
+#endif
