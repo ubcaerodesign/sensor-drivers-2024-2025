ed3f9afcb8e3081f6c6ef613e13ac3dec817b77c
Ryan Cheng <ryan.zy.cheng@gmail.com>
2025-04-08 21:11:57 -0700

    add dma to spi.h

--- ARDUCAM/Inc/spi.h
+++ ARDUCAM/Inc/spi.h
@@ -10,6 +10,10 @@
 #define __SPI_H
 #include "stm32h7xx_hal.h"
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 uint8_t arducamSpiTransfer(uint8_t TxData);
 void Camera_WriteRegister(uint8_t regAddr, uint8_t value);
@@ -17,4 +21,13 @@ uint8_t Camera_ReadRegister(uint8_t regAddr);
 
 void arducamSpiCsPinLow(int pin);
 void arducamSpiCsPinHigh(int pin);
+
+//dma stuff :(
+void SPI_DMA_Init(void);
+HAL_StatusTypeDef Camera_WriteBurstDMA(uint8_t* txData, uint16_t size);
+HAL_StatusTypeDef Camera_ReadBurstDMA(uint8_t* rxData, uint16_t size);
+HAL_StatusTypeDef Camera_TransmitReceiveDMA(uint8_t* txData, uint8_t* rxData, uint16_t size);
+bool SPI_DMA_IsBusy(void);
+void SPI_DMA_WaitForCompletion(void);
+
 #endif
