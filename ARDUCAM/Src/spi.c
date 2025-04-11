60dfdcdbace6fa4f05ca36d8b2a95e8603434424
Ryan Cheng <ryan.zy.cheng@gmail.com>
2025-04-10 18:32:02 -0700

    oops mispaste

--- ARDUCAM/Src/spi.c
+++ ARDUCAM/Src/spi.c
@@ -21,7 +21,7 @@ void SPI_DMA_Init(void)
     /* Make sure DMA streams are disabled */
     HAL_DMA_DeInit(&hdma_spi3_tx);
     HAL_DMA_DeInit(&hdma_spi3_rx);
-    
+
     /* Initialize flags */
     spi_dma_tx_complete = true;
     spi_dma_rx_complete = true;
@@ -94,7 +94,7 @@ bool SPI_DMA_IsBusy(void)
 void SPI_DMA_WaitForCompletion(void)
 {
     uint32_t timeout = HAL_GetTick() + dma_timeout;
-    
+
     while (SPI_DMA_IsBusy()) {
         if (HAL_GetTick() >= timeout) {
             printf("SPI DMA timeout occurred\n");
@@ -163,6 +163,97 @@ uint8_t Camera_ReadRegister(uint8_t regAddr) {
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);   // CS High
 
     return received[1]; // Return the second byte (register value)
+}
+
+/**
+ * @brief  Write data using DMA
+ * @param  txData Data buffer to transmit
+ * @param  size Number of bytes to transmit
+ * @retval HAL status
+ */
+HAL_StatusTypeDef Camera_WriteBurstDMA(uint8_t* txData, uint16_t size)
+{
+    // Make sure any pending DMA transfer is complete
+    SPI_DMA_WaitForCompletion();
+
+    spi_dma_tx_complete = false;
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET); // CS Low
+    HAL_StatusTypeDef status = HAL_SPI_Transmit_DMA(&hspi3, txData, size);
+
+    if (status != HAL_OK) {
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET); // CS High
+        spi_dma_tx_complete = true;
+        printf("DMA Transmit error: %d\n", status);
+    }
+
+    return status;
+}
+
+/**
+ * @brief  Read data using DMA
+ * @param  rxData Buffer to receive data
+ * @param  size Number of bytes to read
+ * @retval HAL status
+ */
+HAL_StatusTypeDef Camera_ReadBurstDMA(uint8_t* rxData, uint16_t size)
+{
+    uint8_t* dummy_tx = NULL;
+    HAL_StatusTypeDef status;
+
+    // Make sure any pending DMA transfer is complete
+    SPI_DMA_WaitForCompletion();
+
+    // Allocate dummy TX buffer filled with 0
+    dummy_tx = (uint8_t*)malloc(size);
+    if (dummy_tx == NULL) {
+        printf("Failed to allocate memory for DMA read\n");
+        return HAL_ERROR;
+    }
+
+    memset(dummy_tx, 0, size);
+
+    spi_dma_rx_complete = false;
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET); // CS Low
+    status = HAL_SPI_TransmitReceive_DMA(&hspi3, dummy_tx, rxData, size);
+
+    if (status != HAL_OK) {
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET); // CS High
+        spi_dma_rx_complete = true;
+        printf("DMA Receive error: %d\n", status);
+    }
+
+    free(dummy_tx);
+    return status;
+}
+
+/**
+ * @brief  Transmit and receive data using DMA
+ * @param  txData Data buffer to transmit
+ * @param  rxData Buffer to receive data
+ * @param  size Number of bytes to transfer
+ * @retval HAL status
+ */
+HAL_StatusTypeDef Camera_TransmitReceiveDMA(uint8_t* txData, uint8_t* rxData, uint16_t size)
+{
+    // Make sure any pending DMA transfer is complete
+    SPI_DMA_WaitForCompletion();
+
+    spi_dma_tx_complete = false;
+    spi_dma_rx_complete = false;
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET); // CS Low
+    HAL_StatusTypeDef status = HAL_SPI_TransmitReceive_DMA(&hspi3, txData, rxData, size);
+
+    if (status != HAL_OK) {
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET); // CS High
+        spi_dma_tx_complete = true;
+        spi_dma_rx_complete = true;
+        printf("DMA TransmitReceive error: %d\n", status);
+    }
+
+    return status;
 }
 
 void arducamSpiCsPinLow(int pin)
